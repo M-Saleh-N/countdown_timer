6fa687817643364562e1d967b04255d78b4abf04
M-Saleh-N <salehnasser65784@gmail.com>
2025-04-26 11:17:13 +0300

    addded

--- src/components/timer.js
+++ src/components/timer.js
@@ -1,6 +1,19 @@
-import React, {useState} from "react";
-import './timer.css'
+import { useState, useEffect } from "react";
 
-function Timer (){
-    
+function Timer() {
+  const [seconds, setSeconds] = useState(60);
+
+  useEffect(() => {
+    if (seconds <= 0) return;
+
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [seconds]);
+
+
 }
+
+export default Timer;
